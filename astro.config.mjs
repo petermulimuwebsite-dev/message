4a7fba0f4526ec844c89f1b7f7cf61322b97b5dc
petermulimuwebsite-dev <petermulimuwebsite@gmail.com>
2026-06-18 18:40:12 +0300

    d

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,14 @@
 import { defineConfig } from 'astro/config';
+import vercel from '@astrojs/vercel/serverless';
+
 
 export default defineConfig({
   // No adapter needed for Vercel static output
   // If you want SSR, add: import vercel from '@astrojs/vercel/serverless';
   // and set: adapter: vercel()
+
+    output: 'hybrid',
+      adapter: vercel(),
+
+
 });
